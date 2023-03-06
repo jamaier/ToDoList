af6ad4b843a072f8a1a533e870844b6c5676a44d
Jacob Maier <jacobamaier@gmail.com>
2023-03-06 15:28:38 -0800

    Update working project to lessons.

--- ToDoList/Controller/CategoriesController.cs
+++ ToDoList/Controller/CategoriesController.cs
@@ -1,94 +1,94 @@
-using System.Collections.Generic;
-using System;
-using Microsoft.AspNetCore.Mvc;
-using ToDoList.Models;
+// using System.Collections.Generic;
+// using System;
+// using Microsoft.AspNetCore.Mvc;
+// using ToDoList.Models;
 
-namespace ToDoList.Controllers
-{
-  public class CategoriesController : Controller
-  {
-    [HttpGet("/categories")]
-    public ActionResult Index()
-    {
-      List<Category> allCategories = Category.GetAll();
-      return View(allCategories);
-    }
+// namespace ToDoList.Controllers
+// {
+//   public class CategoriesController : Controller
+//   {
+//     [HttpGet("/categories")]
+//     public ActionResult Index()
+//     {
+//       List<Category> allCategories = Category.GetAll();
+//       return View(allCategories);
+//     }
 
-    [HttpGet("/categories/new")]
-    public ActionResult New()
-    {
-      return View();
-    }
+//     [HttpGet("/categories/new")]
+//     public ActionResult New()
+//     {
+//       return View();
+//     }
 
-    [HttpPost("/categories")]
-    public ActionResult Create(string categoryName)
-    {
-      Category newCategory = new Category(categoryName);
-      return RedirectToAction("Index");
-    }
+//     [HttpPost("/categories")]
+//     public ActionResult Create(string categoryName)
+//     {
+//       Category newCategory = new Category(categoryName);
+//       return RedirectToAction("Index");
+//     }
 
-    [HttpGet("/categories/{id}")]
-    public ActionResult Show(int id)
-    {
-      Dictionary<string, object> model = new Dictionary<string, object>();
-      Category selectedCategory = Category.Find(id);
-      List<Item> categoryItems = selectedCategory.Items;
-      model.Add("category", selectedCategory);
-      model.Add("items", categoryItems);
-      return View(model);
-    }
+//     [HttpGet("/categories/{id}")]
+//     public ActionResult Show(int id)
+//     {
+//       Dictionary<string, object> model = new Dictionary<string, object>();
+//       Category selectedCategory = Category.Find(id);
+//       List<Item> categoryItems = selectedCategory.Items;
+//       model.Add("category", selectedCategory);
+//       model.Add("items", categoryItems);
+//       return View(model);
+//     }
 
-    // This one creates new Items within a given Category, not new Categories:
-    [HttpPost("/categories/{categoryId}/items")]
-    public ActionResult Create(int categoryId, string itemDescription)
-    {
-      Dictionary<string, object> model = new Dictionary<string, object>();
-      Category foundCategory = Category.Find(categoryId);
-      Item newItem = new Item(itemDescription);
-      foundCategory.AddItem(newItem);
-      List<Item> categoryItems = foundCategory.Items;
-      model.Add("items", categoryItems);
-      model.Add("category", foundCategory);
-      return View("Show", model);
-    }
+//     // This one creates new Items within a given Category, not new Categories:
+//     [HttpPost("/categories/{categoryId}/items")]
+//     public ActionResult Create(int categoryId, string itemDescription)
+//     {
+//       Dictionary<string, object> model = new Dictionary<string, object>();
+//       Category foundCategory = Category.Find(categoryId);
+//       Item newItem = new Item(itemDescription);
+//       foundCategory.AddItem(newItem);
+//       List<Item> categoryItems = foundCategory.Items;
+//       model.Add("items", categoryItems);
+//       model.Add("category", foundCategory);
+//       return View("Show", model);
+//     }
 
-    [HttpPost("/categories/{categoryId}/items/{itemId}/delete")]
+//     [HttpPost("/categories/{categoryId}/items/{itemId}/delete")]
 
-    //should be a model method probably
-    public ActionResult Delete(int categoryId, int itemId)
-    {
-      Category selectedCategory = Category.Find(categoryId);
-      Item selectedCategoryItem = selectedCategory.Items.Find(item => item.Id == itemId);
+//     //should be a model method probably
+//     public ActionResult Delete(int categoryId, int itemId)
+//     {
+//       Category selectedCategory = Category.Find(categoryId);
+//       Item selectedCategoryItem = selectedCategory.Items.Find(item => item.Id == itemId);
 
-      foreach (Item item in selectedCategory.Items)
-      {
-        if (item.Id == itemId)
-        {
-          selectedCategory.Items.Remove(item);
-          break;
-        }
-      }
+//       foreach (Item item in selectedCategory.Items)
+//       {
+//         if (item.Id == itemId)
+//         {
+//           selectedCategory.Items.Remove(item);
+//           break;
+//         }
+//       }
 
-      return RedirectToAction("Show", new { id = categoryId });
-    }
+//       return RedirectToAction("Show", new { id = categoryId });
+//     }
 
-    // public ActionResult DeleteAll(int categoryId)
-    // {
-    // // Item.ClearAll();
-    // Category selectedCategory = Category.Find(categoryId);
-    // List<Item> categoryItems = selectedCategory.Items;
-    // categoryItems.Clear();
+//     // public ActionResult DeleteAll(int categoryId)
+//     // {
+//     // // Item.ClearAll();
+//     // Category selectedCategory = Category.Find(categoryId);
+//     // List<Item> categoryItems = selectedCategory.Items;
+//     // categoryItems.Clear();
 
-    // Dictionary<string, object> model = new Dictionary<string, object>();
-    // model.Remove("category");
-    // model.Remove("item");
+//     // Dictionary<string, object> model = new Dictionary<string, object>();
+//     // model.Remove("category");
+//     // model.Remove("item");
 
-    // return View(model);
-    // model.Clear("category", selectedCategory); // categoryItems
-    // return View();
-    // }
+//     // return View(model);
+//     // model.Clear("category", selectedCategory); // categoryItems
+//     // return View();
+//     // }
 
-    // var item = myList.Find(x => x.ItemName == obj.ItemName);
-    // myList.Remove(item);
-  }
-}
+//     // var item = myList.Find(x => x.ItemName == obj.ItemName);
+//     // myList.Remove(item);
+//   }
+// }
--- ToDoList/Controller/ItemController.cs
+++ ToDoList/Controller/ItemController.cs
@@ -1,50 +1,42 @@
 using Microsoft.AspNetCore.Mvc;
 using ToDoList.Models;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ToDoList.Controllers
 {
   public class ItemsController : Controller
   {
-    //Not necessary
-    // [HttpGet("/items")]
-    // public ActionResult Index()
-    // {
-    //   List<Item> allItems = Item.GetAll();
-    //   return View(allItems);
-    // }
+    private readonly ToDoListContext _db;
 
-    [HttpGet("/categories/{categoryId}/items/new")]
-    public ActionResult New(int categoryId)
+    public ItemsController(ToDoListContext db)
     {
-      Category category = Category.Find(categoryId);
-      return View(category);
+      _db = db;
     }
 
-    //Not Necessary
-    // [HttpPost("/items")]
-    // public ActionResult Create(string description)
-    // {
-    //   Item myItem = new Item(description);
-    //   return RedirectToAction("Index");
-    // }
-
-    // [HttpPost("/items/delete")]
-    // public ActionResult DeleteAll()
-    // {
-    //   Item.ClearAll();
-    //   return View();
-    // }
-
-    [HttpGet("/categories/{categoryId}/items/{itemId}")]
-    public ActionResult Show(int categoryId, int itemId)
+    public ActionResult Index()
     {
-      Item item = Item.Find(itemId);
-      Category category = Category.Find(categoryId);
-      Dictionary<string, object> model = new Dictionary<string, object>();
-      model.Add("item", item);
-      model.Add("category", category);
+      List<Item> model = _db.Items.ToList();
       return View(model);
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Item item)
+    {
+      _db.Items.Add(item);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      Item thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
+      return View(thisItem);
     }
   }
 }
