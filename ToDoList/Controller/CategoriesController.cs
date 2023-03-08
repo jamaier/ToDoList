505d041f213cb0cfe5e468a21e6424bfdcfcac60
Jacob Maier <jacobamaier@gmail.com>
2023-03-08 08:20:05 -0800

    Update homework

--- ToDoList/Controller/CategoriesController.cs
+++ ToDoList/Controller/CategoriesController.cs
@@ -1,94 +1,74 @@
-// using System.Collections.Generic;
-// using System;
-// using Microsoft.AspNetCore.Mvc;
-// using ToDoList.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+using System.Collections.Generic;
+using System.Linq;
 
-// namespace ToDoList.Controllers
-// {
-//   public class CategoriesController : Controller
-//   {
-//     [HttpGet("/categories")]
-//     public ActionResult Index()
-//     {
-//       List<Category> allCategories = Category.GetAll();
-//       return View(allCategories);
-//     }
+namespace ToDoList.Controllers
+{
+  public class CategoriesController : Controller
+  {
+    private readonly ToDoListContext _db;
 
-//     [HttpGet("/categories/new")]
-//     public ActionResult New()
-//     {
-//       return View();
-//     }
+    public CategoriesController(ToDoListContext db)
+    {
+      _db = db;
+    }
 
-//     [HttpPost("/categories")]
-//     public ActionResult Create(string categoryName)
-//     {
-//       Category newCategory = new Category(categoryName);
-//       return RedirectToAction("Index");
-//     }
+    public ActionResult Index()
+    {
+      List<Category> model = _db.Categories.ToList();
+      return View(model);
+    }
 
-//     [HttpGet("/categories/{id}")]
-//     public ActionResult Show(int id)
-//     {
-//       Dictionary<string, object> model = new Dictionary<string, object>();
-//       Category selectedCategory = Category.Find(id);
-//       List<Item> categoryItems = selectedCategory.Items;
-//       model.Add("category", selectedCategory);
-//       model.Add("items", categoryItems);
-//       return View(model);
-//     }
+    public ActionResult Create()
+    {
+      return View();
+    }
 
-//     // This one creates new Items within a given Category, not new Categories:
-//     [HttpPost("/categories/{categoryId}/items")]
-//     public ActionResult Create(int categoryId, string itemDescription)
-//     {
-//       Dictionary<string, object> model = new Dictionary<string, object>();
-//       Category foundCategory = Category.Find(categoryId);
-//       Item newItem = new Item(itemDescription);
-//       foundCategory.AddItem(newItem);
-//       List<Item> categoryItems = foundCategory.Items;
-//       model.Add("items", categoryItems);
-//       model.Add("category", foundCategory);
-//       return View("Show", model);
-//     }
+    [HttpPost]
+    public ActionResult Create(Category category)
+    {
+      _db.Categories.Add(category);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
 
-//     [HttpPost("/categories/{categoryId}/items/{itemId}/delete")]
+    public ActionResult Details(int id)
+    {
+      Category thisCategory = _db.Categories
+                                  .Include(category => category.Items)
+                                  .FirstOrDefault(category => category.CategoryId == id);
+      return View(thisCategory);
+    }
 
-//     //should be a model method probably
-//     public ActionResult Delete(int categoryId, int itemId)
-//     {
-//       Category selectedCategory = Category.Find(categoryId);
-//       Item selectedCategoryItem = selectedCategory.Items.Find(item => item.Id == itemId);
+    public ActionResult Edit(int id)
+    {
+      Category thisCategory = _db.Categories.FirstOrDefault(category => category.CategoryId == id);
+      return View(thisCategory);
+    }
 
-//       foreach (Item item in selectedCategory.Items)
-//       {
-//         if (item.Id == itemId)
-//         {
-//           selectedCategory.Items.Remove(item);
-//           break;
-//         }
-//       }
+    [HttpPost]
+    public ActionResult Edit(Category category)
+    {
+      _db.Categories.Update(category);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
 
-//       return RedirectToAction("Show", new { id = categoryId });
-//     }
+    public ActionResult Delete(int id)
+    {
+      Category thisCategory = _db.Categories.FirstOrDefault(category => category.CategoryId == id);
+      return View(thisCategory);
+    }
 
-//     // public ActionResult DeleteAll(int categoryId)
-//     // {
-//     // // Item.ClearAll();
-//     // Category selectedCategory = Category.Find(categoryId);
-//     // List<Item> categoryItems = selectedCategory.Items;
-//     // categoryItems.Clear();
-
-//     // Dictionary<string, object> model = new Dictionary<string, object>();
-//     // model.Remove("category");
-//     // model.Remove("item");
-
-//     // return View(model);
-//     // model.Clear("category", selectedCategory); // categoryItems
-//     // return View();
-//     // }
-
-//     // var item = myList.Find(x => x.ItemName == obj.ItemName);
-//     // myList.Remove(item);
-//   }
-// }
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      Category thisCategory = _db.Categories.FirstOrDefault(category => category.CategoryId == id);
+      _db.Categories.Remove(thisCategory);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+  }
+}
